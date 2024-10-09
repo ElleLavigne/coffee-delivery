2bba4b917d5e76375b3fe3da573e18f0dc0d8a27
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2024-10-09 17:27:00 -0300

    feat hero section

--- src/home/Hero/index.tsx
+++ src/home/Hero/index.tsx
@@ -1,14 +1,73 @@
-import { Container } from "../../components/Header/styles";
-import { Title } from "./styles";
+import { ShoppingCart, Package, Timer, Coffee } from "phosphor-react"
+import { BaseIcon, Content, Img, Item, ItemsContainer, TitleContainer, Container } from "./styles"
+import { defaultTheme } from "../../styles/themes/default"
 
+
+interface Item {
+  name: string
+  Icon: typeof ShoppingCart
+  weight?: "fill" | "regular"
+  bgColor: string
+  text: string
+}
+
+const items: Item[] = [
+  {
+    name: "ShoppingCart",
+    Icon: ShoppingCart,
+    weight: "fill",
+    bgColor: defaultTheme["yellow-500"],
+    text: "Compra simples e segura",
+  },
+  {
+    name: "Box",
+    Icon: Package,
+    weight: "fill",
+    bgColor: defaultTheme["gray-500"],
+    text: "Embalagem mantém o café intacto",
+  },
+  {
+    name: "Timer",
+    Icon: Timer,
+    weight: "fill",
+    bgColor: defaultTheme["yellow-300"],
+    text: "Entrega rápida e rastreada",
+  },
+  {
+    name: "Coffee",
+    Icon: Coffee,
+    weight: "fill",
+    bgColor: defaultTheme["purple-500"],
+    text: "O café chega fresquinho até você",
+  },
+]
 export function Hero() {
   return (
     <Container>
-      <img src="hero-img.png" alt="" />
-      <Title>
-      <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-      <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora </p>
-      </Title>
+      <Content>
+        <TitleContainer>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora{" "}
+          </p>
+        </TitleContainer>
+
+        <ItemsContainer>
+          {items.map((item) => {
+            const { bgColor, Icon, name, weight, text } = item
+            return (
+              <Item key={name}>
+                <BaseIcon style={{ backgroundColor: bgColor }}>
+                  <Icon weight={weight} size={22} />
+                </BaseIcon>
+                <p>{text}</p>
+              </Item>
+            )
+          })}
+        </ItemsContainer>
+      </Content>
+      <Img src="hero-img.png" alt="" />
     </Container>
   )
 }
--- src/home/Hero/styles.ts
+++ src/home/Hero/styles.ts
@@ -3,10 +3,19 @@ import styled from "styled-components"
 export const Container = styled.div`
   max-width: 70rem;
   margin: 0 auto;
+
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  padding: 5.75rem 0;
 `
-
-
-export const Title = styled.div`
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4.125rem;
+ 
+`
+export const TitleContainer = styled.div`
   max-width: 36.75rem;
   display: flex;
   flex-direction: column;
@@ -14,9 +23,43 @@ export const Title = styled.div`
   p {
     font-family: "Roboto", sans-serif;
     font-size: 1.25rem;
+    color: ${(props) => props.theme["gray-300"]};
   }
   h1 {
     font-family: "Baloo 2", sans-serif;
+    font-weight: 900;
+    line-height: 130%;
     font-size: 3rem;
+    color: ${(props) => props.theme["gray-500"]};
   }
+`
+export const Img = styled.img`
+  width: 476px;
+  
+`
+export const ItemsContainer = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr;
+gap: 1.25rem;
+`
+
+export const BaseIcon = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 9999px;
+  color: ${(props) => props.theme["white-100"]};
+  
+`
+export const Item = styled.div`
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  gap: 0.75rem;
+
+  font-family: "Roboto", sans-serif;
+  font-weight: 500;
+  color: ${(props) => props.theme["gray-300"]};
 `
